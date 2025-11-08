3156259876046181f63122d66dba3034104fb8a3
Eman Celo <celoeman@gmail.com>
2025-11-07 22:18:37 -0500

    saving

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -22,16 +22,7 @@ export default function SmoothScroll({
         ).matches;
         if (prefersReducedMotion) return;
 
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            orientation: 'vertical',
-            gestureOrientation: 'vertical',
-            smoothWheel: true,
-            wheelMultiplier: 1,
-            touchMultiplier: 2,
-            infinite: false,
-        });
+        const lenis = new Lenis();
 
         lenisRef.current = lenis;
 
